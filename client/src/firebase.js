2939c234df949e188eca4afe600973eb193248f1
Jay-Deeee <144668463+Jay-Deeee@users.noreply.github.com>
2026-06-02 13:36:32 +0800

    Initial React frontend setup

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "YAIzaSyAfZO2Ctj62iqsd1pYky-hLr-QEy_w4mTA",
+  authDomain: "mini-hcm-5de49.firebaseapp.com",
+  projectId: "mini-hcm-5de49",
+  storageBucket: "mini-hcm-5de49.firebasestorage.app",
+  messagingSenderId: "183077903750",
+  appId: "1:183077903750:web:c371bea473894402f9e007",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
